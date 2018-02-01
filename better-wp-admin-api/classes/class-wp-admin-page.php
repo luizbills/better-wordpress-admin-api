5e5b5c0fa9db5f739362e925403131c42cea72c9
Luiz Bills <luizpbills@gmail.com>
2018-02-01 13:53:34 +0000

    fix 'Undefined index' notice

--- better-wp-admin-api/classes/class-wp-admin-page.php
+++ better-wp-admin-api/classes/class-wp-admin-page.php
@@ -507,6 +507,8 @@ class _WP_Admin_Page {
 		$value = '';
 
 		foreach ( $possible_ids as $_id ) {
+			if ( ! isset( $this->fields[ $_id ] ) ) continue;
+
 			$field = $this->fields[ $_id ];
 
 			if ( ! empty( $field ) ) {
