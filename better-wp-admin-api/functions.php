f47e2d7c21feb884d6883385df0a85b43250e857
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-02-02 17:01:21 -0200

    Update functions.php

--- better-wp-admin-api/functions.php
+++ better-wp-admin-api/functions.php
@@ -6,7 +6,7 @@ function wp_create_admin_page ( $settings ) {
     return new _WP_Admin_Page( $settings );
 }
 
-function wp_get_page_instance ( $page_id = '' ) {
+function wp_get_admin_page ( $page_id = '' ) {
     return _WP_Admin_Page::get_page_instance( $page_id );
 }
 
@@ -17,5 +17,10 @@ function wp_get_page_field_value ( $page_id, $field_id ) {
         return $the_page->get_field_value( $field_id );
     }
 
+    return false;
+}
+
+function wp_get_metabox_field_value ( $metabox_id, $field_id ) {
+    // TODO
     return false;
 }
