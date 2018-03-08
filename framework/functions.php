edd9d5b0d5c73d562ba0b29223f09752e557ff10
Luiz Bills <luizpbills@gmail.com>
2018-03-07 22:02:00 -0300

    fixed indentation

--- framework/functions.php
+++ framework/functions.php
@@ -3,24 +3,24 @@
 if ( ! defined( 'WPINC' ) ) die;
 
 function wp_create_admin_page ( $settings ) {
-    return new _WP_Admin_Page( $settings );
+	return new _WP_Admin_Page( $settings );
 }
 
 function wp_get_admin_page ( $page_id = '' ) {
-    return _WP_Admin_Page::get_instance_by_id( $page_id );
+	return _WP_Admin_Page::get_instance_by_id( $page_id );
 }
 
 function wp_get_page_field_value ( $page_id, $field_id ) {
-    $the_page = _WP_Admin_Page::get_instance_by_id( $page_id );
+	$the_page = _WP_Admin_Page::get_instance_by_id( $page_id );
 
-    if ( ! empty( $the_page ) ) {
-        return $the_page->get_field_value( $field_id );
-    }
+	if ( ! empty( $the_page ) ) {
+		return $the_page->get_field_value( $field_id );
+	}
 
-    return false;
+	return false;
 }
 
 function wp_get_metabox_field_value ( $metabox_id, $field_id ) {
-    // TODO
-    return false;
+	// TODO
+	return false;
 }
